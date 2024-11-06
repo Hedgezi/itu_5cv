97627d4ff6de2d723a158f020996595d8c94196d
MV <nikola@rootnix.net>
2024-11-06 13:56:49 +0100

    done

---
+++ WPFWeather.App/Windows/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using WPFWeather.App.Services;
+using WPFWeather.App.ViewModels;
+
+namespace WPFWeather.App.Windows
+{
+    public partial class MainWindow : Window
+    {
+        WeatherViewModel viewModel = new WeatherViewModel(new WeatherDiskService());
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel.DownloadWeatherCommand.Execute("Brno");
+
+            DataContext = viewModel;
+        }
+    }
+}
